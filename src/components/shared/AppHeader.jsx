7537da640af8f24da87c6c7d73d729cf72b2c89b
daussfajar <daussfajar28@gmail.com>
2024-08-20 17:12:59 +0700

    Update Portofolio

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -80,7 +80,7 @@ const AppHeader = () => {
 				>
 					<Link
 						to="/"
-						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
+						className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 						aria-label="Home"
 					>
 						Home
