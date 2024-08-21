dbb0a169eb8ca7ac26ff253c8ff99e2eeaa449b2
daussfajar <daussfajar28@gmail.com>
2024-08-21 14:37:42 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
 import AppHeader from './components/shared/AppHeader';
